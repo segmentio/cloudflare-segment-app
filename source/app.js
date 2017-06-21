9989128cc3fc3136caa8c2ccd56b2260e8e2a6fd
Teffen Ellis (Eric) <teffen@nirri.us>
2017-06-21 13:03:00 -0700

    Flesh out click tracking.

--- source/app.js
+++ source/app.js
@@ -1,6 +1,12 @@
 (function () {
   var options = INSTALL_OPTIONS
 
+  var TAG_LABELS = {
+    DEFAULT: 'Element',
+    A: 'Link',
+    BUTTON: 'Button'
+  }
+
   if (!options.writeKey) return
 
   window.analytics.load(options.writeKey)
@@ -8,4 +14,21 @@
   if (options.autoPage) {
     window.analytics.page()
   }
+
+  options.trackedActions.forEach(function (trackedAction) {
+    var element = document.querySelector(trackedAction.location)
+
+    if (!element) return
+
+    var style = document.defaultView.getComputedStyle(element)
+
+    analytics.trackLink(link, trackedAction.label, {
+      location: trackedAction.location,
+      color: JSON.stringify({text: style.color, background: style.backgroundColor}),
+      text: element.textContent,
+      CTA: element.textContent,
+      type: TAG_LABELS[element.tagName] || TAG_LABELS.DEFAULT,
+      pageTitle: document.title
+    });
+  })
 })();
