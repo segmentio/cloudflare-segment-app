2547da20d644ca0d2eccde6914f81ed787d1f7fa
Fouad Matin <m@fouad.co>
2017-06-25 17:03:21 -0700

    add screenshots, remove point-click tracking

--- source/app.js
+++ source/app.js
@@ -1,15 +1,6 @@
-(function () {
+;(function() {
   'use strict'
-
-  if (!document.addEventListener) return // Check for IE9+
-
   var options = INSTALL_OPTIONS
-
-  var TAG_LABELS = {
-    DEFAULT: 'Element',
-    A: 'Link',
-    BUTTON: 'Button'
-  }
 
   if (!options.writeKey) return
 
@@ -18,21 +9,4 @@
   if (options.autoPage) {
     window.analytics.page()
   }
-
-  options.trackedActions.forEach(function (trackedAction) {
-    var element = document.querySelector(trackedAction.location)
-
-    if (!element) return
-
-    var style = document.defaultView.getComputedStyle(element)
-
-    analytics.trackLink(link, trackedAction.label, {
-      location: trackedAction.location,
-      color: JSON.stringify({text: style.color, background: style.backgroundColor}),
-      text: element.textContent,
-      CTA: element.textContent,
-      type: TAG_LABELS[element.tagName] || TAG_LABELS.DEFAULT,
-      pageTitle: document.title
-    })
-  })
-}())
+})()
